66f0e5b58d8f768ccd0a09e6fcd041b8cfdd0dc1
emmacarenaa5 <117159781+emmacarenaa5@users.noreply.github.com>
2023-03-10 10:58:45 +0100

    Add files via upload

---
+++ A.php
@@ -0,0 +1,100 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title> A </title>
+        <link rel="stylesheet" href="Acceuil CSS.css">
+    </head>
+
+    <body>
+        <a href="Index.php" style = "text-decoration : none" > <h1>WIKI SPORT </h1> </a>
+        <table  align="right" >
+                <td class="cell">
+                    <h5 class="conins"  ><br> Connexion / Inscription</h5> 
+                </td>
+        </table> <br><br>
+        <br> <br>
+        <Form>
+            <form method="post">
+                <input type="text" placeholder="Rechercher un sport">
+        </FORm> 
+        
+        <table>
+            <tr>
+                <td rowspan="2"<form class="cara"methode="post">
+                    <h5> <strong> <u> Listes des caractéristiques :</u> </strong><br> <br>
+                    <input type="checkbox" name="Sport Collectif" id="Sport Collectif" > 
+                    <label>Sport Collectif           </label> <br>
+                    <input type="checkbox" name="Sport Individuel" id="Sport Individuel" > 
+                    <label>Sport Individuel      </label> <br>
+                    <input type="checkbox" name="Sport Intérieur" id="Sport Intérieur" > 
+                    <label>Sport Intérieur       </label> <br>
+                    <input type="checkbox" name="Sport Extérieur" id="Sport Extérieur" > 
+                    <label>Sport Extérieur       </label> <br>
+                    <input type="checkbox" name="Sport de Ballon" id="Sport de Ballon" > 
+                    <label>Sport de Ballon       </label> <br>
+                    <input  type="checkbox" name=" Sport de Rapidité" id=" Sport de Rapidité" > 
+                    <label> Sport de Rapidité    </label> <br>
+                    <input type="checkbox" name=" Sport avec Animaux" id=" Sport avec Animaux" > 
+                    <label> Sport avec Animaux    </label> <br>
+                    <input type="checkbox" name="Sport Aquatique" id="Sport Aquatique " > 
+                    <label>Sport Aquatique       </label><br>
+                    <input type="checkbox" name=" Sport de Course   " id=" Sport de Course   " > 
+                    <label> Sport de Course    </label> <br>
+                    <input type="checkbox" name=" Danse " id=" Danse " > 
+                    <label> Danse                </label> <br>
+                    <input type="checkbox" name=" Relaxation  " id=" Relaxation  " > 
+                    <label> Relaxation   </label> <br> <br>
+                    <label>Popularité du sport   </label>
+                    <input type="range" name="Popularité du sport" id="Popularité du sport " > <br>
+                    
+                </form> </td>
+                <td> 
+                   <h3> <strong class="lettre">A </strong> B C D E F G H I J K L M N O P Q R S T U V W X Y Z </h3> </td>
+            </tr>
+            <tr>
+                <td> 
+                    <article>
+                        <table class="tabsport" border="3px" align="center">
+                            <tr>
+                               <td rowspan="2">
+                                 <img src="chat.jpg" alt="Miss teigne" height="100%" width="100%">
+                                </td>
+                             
+                                <td>
+                                    <h2><i>"Nom d'un sport" </i> </h2>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>
+                                    <i> "Description du sport selon les informations données dans la base de données
+                                        Quam ob rem cave Catoni anteponas ne istum quidem ipsum, quem Apollo, ut ais, sapientissimum iudicavit; huius enim facta, illius dicta laudantur. De me autem, ut iam cum utroque vestrum loquar, sic habetote.
+                                        Isdem diebus Apollinaris Domitiani gener, paulo ante agens palatii Caesaris curam, ad Mesopotamiam missus a socero per militares numeros immodice scrutabatur, an quaedam altiora meditantis iam Galli secreta susceperint scripta, qui conpertis Antiochiae gestis per minorem Armeniam lapsus Constantinopolim petit exindeque per protectores retractus artissime tenebatur.
+                                    </i>
+                                </p> 
+                            </td> 
+                            </tr>
+                    </table>  
+                    </article>
+                </td>
+            </tr>
+
+        </table>
+        
+        
+        
+    
+        
+        
+
+    
+    
+    
+
+        
+        
+    
+    </h5>
+    </body>
+
+</html>
